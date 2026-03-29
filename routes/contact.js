8108b1b763b742cd481c48839310e69c18bffc5a
chris almpanis <christoforos.a@outlook.com>
2026-03-29 23:56:01 +0300

    Debugging

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../../../models/Contact');
+const Contact = require('../models/Contact')
 
 // SEND contact message
 router.post('/', async (req, res) => {
